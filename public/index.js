2ae3542e6b12f674cfe048e32102bb9f2f206c86
DrinkTracker <154763093+DrinkTracker@users.noreply.github.com>
2023-12-25 22:28:26 -0600

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,16 +1,3 @@
-function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, "+")
-    .replace(/_/g, "/");
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-}
-
 async function run() {
   // A service worker must be registered in order to send notifications on iOS
   const registration = await navigator.serviceWorker.register(
@@ -21,28 +8,40 @@ async function run() {
   );
 
   const button = document.getElementById("subscribe");
-  button.addEventListener("click", async () => {
-    // Triggers popup to request access to send notifications
-    const result = await window.Notification.requestPermission();
 
-    // If the user rejects the permission result will be "denied"
-    if (result === "granted") {
-      const subscription = await registration.pushManager.subscribe({
-        applicationServerKey: urlBase64ToUint8Array(
-          "BG6cugJoZK2O-Lj-lnTTdHBIxMIEjkHveTsFSYg6xfxIUWJtJ1CooinqRepotUhBbyvZXi73IQzcnFjOT8e7XPQ"
-        ),
-        userVisibleOnly: true,
-      });
+  const areNotificationsGranted = window.Notification.permission === "granted";
+  if (areNotificationsGranted) {
+    button.innerText = "Send Notification";
 
-      await fetch("/save-subscription", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(subscription),
-      });
-    }
-  });
+    button.addEventListener("click", async () => {
+      await fetch("/send-notification");
+    });
+  } else {
+    button.addEventListener("click", async () => {
+      // Triggers popup to request access to send notifications
+      const result = await window.Notification.requestPermission();
+
+      // If the user rejects the permission result will be "denied"
+      if (result === "granted") {
+        const subscription = await registration.pushManager.subscribe({
+          // TODO: Replace with your public vapid key
+          applicationServerKey:
+            "BG6cugJoZK2O-Lj-lnTTdHBIxMIEjkHveTsFSYg6xfxIUWJtJ1CooinqRepotUhBbyvZXi73IQzcnFjOT8e7XPQ",
+          userVisibleOnly: true,
+        });
+
+        await fetch("/save-subscription", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(subscription),
+        });
+
+        window.location.reload();
+      }
+    });
+  }
 }
 
 run();
