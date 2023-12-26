07a15a4c6d5cc45489ea5c61aab44d0b5ad663ce
DrinkTracker <154763093+DrinkTracker@users.noreply.github.com>
2023-12-25 22:28:40 -0600

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,6 @@
 self.addEventListener("push", async (event) => {
   const { title, body } = await event.data.json();
+  console.log(title, body);
   self.registration.showNotification(title, {
     body,
   });
